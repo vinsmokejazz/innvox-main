d67d12650f336209ae578bc6cc7e3212fe4b2cd4
vinsmokejazz <vinsmokejazz@gmail.com>
2025-04-13 21:55:54 +0530

    Add smooth scrolling to About section on HomePage. Implemented useEffect to scroll into view when the hash is '#about', enhancing in-page navigation experience.

--- innvox-fe/src/pages/HomePage.jsx
+++ innvox-fe/src/pages/HomePage.jsx
@@ -3,8 +3,20 @@ import HeroSection from '../components/home/HeroSection';
 import AboutSection from '../components/home/AboutSection';
 import FeaturesSection from '../components/home/FeaturesSection';
 import CtaSection from '../components/home/CtaSection';
-
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 const HomePage = () => {
+  const location = useLocation();
+  
+  useEffect(() => {
+    if (location.hash === '#about') {
+      const aboutSection = document.getElementById('about');
+      if (aboutSection) {
+        aboutSection.scrollIntoView({ behavior: 'smooth' });
+      } 
+    }
+  }, [location.hash]);    
+  
   return (
     <div className="space-y-20 py-20">
       <HeroSection />
